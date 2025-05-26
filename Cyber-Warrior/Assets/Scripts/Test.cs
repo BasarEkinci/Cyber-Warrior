d5307882a15ec7a8962a41258d87da27b3c31fbb
Başar Ekinci <basarekinci02@outlook.com>
2025-05-26 16:26:14 +0300

    Test Script Updated

--- Cyber-Warrior/Assets/Scripts/Test.cs
+++ Cyber-Warrior/Assets/Scripts/Test.cs
@@ -7,31 +7,40 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    [Header("Test Values")]
     [SerializeField] private float damageAmount = 10f;
     [SerializeField] private int scarpToAdd = 300;
     [SerializeField] private int scarpToSpend = 100;
- 
+    [Header("Class References")]
     [SerializeField] private PlayerHealth playerHealth;
+    [Header("Data References")]
     [SerializeField] private GameState gameState;
     [SerializeField] private GameStateEvent gameStateEvent;
     
-    [Button]
+    [Title("Changes the game state with specified value")]
+    [Button(ButtonSizes.Large)]
     public void ChangeGameState()
     {
         gameStateEvent.RaiseEvent(gameState);
     }
-    [Button]
+    [PropertySpace]
+    [Title("Adds the Scarp up to given value")]
+    [Button(ButtonSizes.Large)]
     public void AddScarp()
     {
         ScarpAmountManager.Instance.AddScarp(scarpToAdd);
     }
-    [Button]
+
+    [PropertySpace]
+    [Title("Spends the Scarp up to given value")]
+    [Button(ButtonSizes.Large)]
     public void SpendScarp()
     {
         ScarpAmountManager.Instance.TrySpendScarp(scarpToSpend);
     }
-    
-    [Button]
+    [PropertySpace]
+    [Title("Damages the player up to given value")]
+    [Button(ButtonSizes.Large)]
     public void DamagePlayer()
     {
         playerHealth.TakeDamage(damageAmount);
