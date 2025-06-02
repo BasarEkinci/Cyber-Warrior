d4e55911702f1ab3e8efa50331affb7945427e7e
BasarEkinci <basarekinci02@outlook.com>
2025-06-02 15:48:34 +0300

    Game Manager Added

---
+++ Cyber-Warrior/Assets/Scripts/Runtime/Extensions/MonoSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Runtime.Extensions
+{
+    public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static readonly Lazy<T> _instance = new Lazy<T>(() =>
+        {
+            var existingInstance = FindObjectOfType<T>();
+            if (existingInstance != null)
+                return existingInstance;
+        
+            GameObject singletonObject = new GameObject(typeof(T).Name);
+            return singletonObject.AddComponent<T>();
+        });
+
+        public static T Instance => _instance.Value;
+
+        protected virtual void Awake()
+        {
+            if (_instance.IsValueCreated && _instance.Value != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Cyber-Warrior/Assets/Scripts/Runtime/Managers/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Runtime.Data.UnityObjects.Events;
+using Runtime.Enums;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private GameStateEvent gameStateEvent;
+
+        private void OnEnable()
+        {
+            gameStateEvent.RaiseEvent(GameState.Base);
+        }
+    }
+}
